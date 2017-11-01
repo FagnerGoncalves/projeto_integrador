71cbb80b0cdcad8151a1624eee3d628398541829
aluno <https://github.com/FagnerGoncalves/projeto_integrador.git>
2017-11-01 17:43:07 -0400

    banco de dados

---
+++ Alunos.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from extensaoportalaluno import app
+
+
+db = SQLAlchemy(app)
+
+class Alunos(db.Model):
+    id = db.Column('aluno_id', db.Integer)
+    nome = db.Column(db.String(100))
